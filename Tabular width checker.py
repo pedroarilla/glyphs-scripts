89cc2381fd6adcbb15eac76dd0286dbb58123c38
Pedro Arilla <pedroarilla@gmail.com>
2017-12-22 19:34:31 +0000

    Report improved

--- Tabular width checker.py
+++ Tabular width checker.py
@@ -16,17 +16,17 @@ differentWidthCounter = 0
 
 Glyphs.clearLog()
 Glyphs.showMacroWindow()
+print "Processing..."
 
 for thisLayer in selectedLayers:
 	width = thisLayer.width
 	if width == masterWidth:
+		print "+++" + thisLayer.parent.name + ": Yep! (%i)" % width
 		sameWidthCounter += 1
 	else:
-		print thisLayer.parent.name + ": Nope!"
+		print "---" + thisLayer.parent.name + ": Nope! (%i)" % width
 		differentWidthCounter += 1
 
-print "====================="
-print "TABULAR WIDTH CHECKER"
-print "====================="
+print "Result:"
 print "%i glyphs are on same width." % sameWidthCounter
 print "%i glyphs are on different width." % differentWidthCounter
